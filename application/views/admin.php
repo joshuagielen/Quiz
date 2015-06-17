290a1ddb1efe227aaf049defbb72a32f6604d09c
HouseK <housenkobe@gmail.com>
2015-06-17 19:41:05 +0200

    admin webpage

---
+++ application/views/admin.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QuizAdmin</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="administration view for the quiz framework">
+    <meta name="author" content="Kobe Housen">
+    <meta name="author" content="Joshua Gielen">
+
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/sb-admin.css" rel="stylesheet" type="text/css">    
+    <link href="assets/css/plugins/morris.css" rel="stylesheet">
+	
+</head>
+<body>
+	<div id="wrapper">
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="navbar-header">
+            	<a class="navbar-brand" href="#">Quiz Admin</a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+            	<ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Rounds <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">Round1</a>
+                            </li>
+                            <li>
+                                <a href="#">Round2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Questions</a>
+                    </li>                   
+                    <li>
+                        <a href="tables.html"><i class="fa fa-fw fa-table"></i> Scores</a>
+                    </li>
+                    <li>
+                        <a href="tables.html"><i class="fa fa-fw fa-table"></i> Clients</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Teams <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">Team1</a>
+                            </li>
+                            <li>
+                                <a href="#">Team2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+                    </li>
+                    <li>
+                        <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+	</div>
+</body>
+</html>
