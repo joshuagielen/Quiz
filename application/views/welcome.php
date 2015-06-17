a562b10b75c3f6a990b982207212788e7da5463f
Joshua Gielen <joshua.gielen@student.pxl.be>
2015-06-17 16:04:50 +0200

    welcome page

---
+++ application/views/welcome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quiz</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	
+</head>
+<body>
+	<nav></nav>
+	<div class="container text-center">
+
+
+	<h1>Welkom</h1>
+
+	<form method="Post" action="#">
+		<input class="btn btn-lg" type="submit" value="Deelnemen aan Quiz">
+	</form>
+
+</div>
+
+</body>
+</html>
