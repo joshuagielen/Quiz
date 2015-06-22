7b775b541c7d066a72ac7dbbf929688f7a84bd34
HouseK <housenkobe@gmail.com>
2015-06-22 16:56:03 +0200

    move Round Order List

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -398,64 +398,61 @@ public function addNewTeam(){
 
   }
 
-    public function newRound(){
-    //set validation rules
-              $this->form_validation->set_rules('roundName', 'roundName', 'required|is_unique[rounds.roundName]');
-              
+  public function newRound(){
+  //set validation rules
+            $this->form_validation->set_rules('roundName', 'roundName', 'required|is_unique[rounds.roundName]');
+            
+            $this->load->model('RoundModel');
+            $roundName = $this->input->post("roundName");            
+            $this->load->model('QuestionQueueModel');
+            $data['qq'] = $this->QuestionQueueModel->getQuestionQueue();
+        
+           
+      //run validation on form input
+            if ($this->form_validation->run() == FALSE )
+            {
+            //validation fails
+              $this->load->helper('url');
+              $data = $this->loadNavData();
+              $this->load->view('admin_nav', $data);
               $this->load->model('RoundModel');
-              $roundName = $this->input->post("roundName");
-          
-             
-        //run validation on form input
-              if ($this->form_validation->run() == FALSE )
-              {
-              //validation fails
-                $this->load->helper('url');
-                $data = $this->loadNavData();
-                $this->load->view('admin_nav', $data);
-                $this->load->model('RoundModel');
-                
-                $this->load->view('admin_add_round');
-                $this->load->view('admin_footer');
-              }
-              else
-              {
-                //Add Round
-                $roundName = $this->input->post("roundName");
               
-                
-                $roundData = array(
-                  "roundName" => $roundName
-                  
-                  );
-                          
-                if ($this->RoundModel->insertRound($roundData) )
-                {    
-                                            
- 
-                // team and players added
-                  $this->session->set_flashdata('roundMsg','<div class="alert alert-success text-center">Round is succesfully added!</div>');
-                  redirect(base_url('/Admin/newRound'));
-              }
-              else
-              {
-                //error
-                $this->session->set_flashdata('roundMsg','<div class="alert alert-danger text-center">Something went wrong!</div>');
-                redirect(base_url('/Admin/newRound'));
-              }
+              $this->load->view('admin_add_round');
+              $this->load->view('admin_footer');
             }
+            else
+            {
+              //Add Round
+              $roundName = $this->input->post("roundName");
+            
+              
+              $roundData = array(
+                "roundName" => $roundName
+                
+                );
+                        
+              if ($this->RoundModel->insertRound($roundData) )
+              {    
+                                          
+
+              // team and players added
+                $this->session->set_flashdata('roundMsg','<div class="alert alert-success text-center">Round is succesfully added!</div>');
+                redirect(base_url('/Admin/newRound'));
+            }
+            else
+            {
+              //error
+              $this->session->set_flashdata('roundMsg','<div class="alert alert-danger text-center">Something went wrong!</div>');
+              redirect(base_url('/Admin/newRound'));
+            }
+          }
   }
 
   public function dashboard(){
 
     $this->load->helper('url');
-    $this->load->model('RoundModel');
-    $this->load->model('QuestionQueueModel');
-
-    $this->load->model('QuestionQueueModel');
+    $this->load->model('StatusModel');
     $data = $this->loadNavData();
-
-    $data['qq'] = $this->QuestionQueueModel->getQuestionQueue();
 
 
     $this->load->view('admin_nav', $data);
@@ -472,5 +469,3 @@ public function addNewTeam(){
     return $data;
   }
 }
-
-
--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -1,36 +1,5 @@
-<div class="row">
-    <div class="col-xs-6">
-      <?php
-        echo "<ul id='roundList' class='list-group text-left sortable'> ";
-        $noSeqRounds = array();
-        $seqRounds = array();
-
-        foreach ($roundsnav as $round){
-        	if ($round->roundSequenceNumber == Null){
-        		array_push($noSeqRounds,$round);
-        	}
-        	else {
-        		$seqRounds[$round->roundSequenceNumber] = $round;
-        	}
-        }
 
 
-        
-        
-        for ($i=0;$i<count($seqRounds);$i++){
-        	echo "<li class='list-group-item' id='" . $seqRounds[$i]->roundId . "'>" . $seqRounds[$i]->roundName  . "</li>";
-        }
-        for ($i=0;$i<count($noSeqRounds);$i++){
-        	echo "<li class='list-group-item' id='" . $noSeqRounds[$i]->roundId . "'>" . $noSeqRounds[$i]->roundName  . "</li>";
-        }
-        
-        echo "</ul>";
-        ?>
-
-
-
-    </div>
-</div>
 <div class="container text-center">
 	<button onclick="forward()">Forward</button>
 </div>
@@ -64,33 +33,5 @@
     	socket.close();
 	});
 </script>
-<script src="<?php echo base_url();?>assets/js/html.sortable.src.js"></script>
-      <script>
-        $(".sortable").sortable({
-          connectWith: '.js-connected'
-        }).bind('sortupdate', function(e, ui) {
-          var quizRoundAmount = document.getElementById("roundList").getElementsByTagName("li").length;
-          var data = new Array();
-          for (i = 0; i < quizRoundAmount; i++) {
-            data[i] = $('#roundList li')[i].id;
-          }
-          
-          var p = {};
-          p['rounds'] = data;
 
-
-
-
-          $.ajax({
-            type:'POST',
-            url:'<?php echo base_url() ?>Rounds/Update',
-            data:p,
-            success:function(data){
-            },
-            error: function(a, b, c) {
-              alert(a + ' ' + b + ' ' + c);
-            }
-        });
-        });
-      </script>
       
