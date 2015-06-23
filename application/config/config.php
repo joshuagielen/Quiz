eeedf9ee8d0e386b96e29f4f0207cfa0d47a4668
HouseK <housenkobe@gmail.com>
2015-06-23 17:03:03 +0200

    y

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/Quiz/';
+$config['base_url'] = 'http://xsz.ddns.net/Quiz/';
 
 
 
